65dd079f1cc65ae0872c5b0165b4a4457b9c1074
Qi Zhang <smallqi1@163.com>
2023-01-10 18:10:32 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 # 判断目录不为空
 if [ -n "$(ls -A /usr/share/elasticsearch/plugins)" ]; then
-    echo "dir is not empty"
+  echo "elasticsearch plugins directory is not empty"
 else
-    cp -r /data/elasticsearch-analysis-ik-7.15.2/* /usr/share/elasticsearch/plugins
+  echo "elasticsearch plugins directory is empty"
+  cp -r /data/elasticsearch-analysis-ik-7.15.2/* /usr/share/elasticsearch/plugins
+  echo "copy elasticsearch plugins success"
 fi
