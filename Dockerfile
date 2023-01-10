51fcfa66be3a59026d9561e1536e443e45ce6688
Qi Zhang <smallqi1@163.com>
2023-01-10 21:03:58 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM alpine:3
 ADD elasticsearch-analysis-ik-7.15.2 /data
 ADD entrypoint.sh /
 
-VOLUME [ "/usr/share/elasticsearch/plugins" ]
-
 CMD sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# 判断目录不为空
-if [ -n "$(ls -A /usr/share/elasticsearch/plugins)" ]; then
+
+if [ ! -d "/usr/share/elasticsearch/plugins/analysis-ik" ]; then
+  echo "elasticsearch plugins directory is not exist"
+  if mkdir -p /usr/share/elasticsearch/plugins/analysis-ik; then
+    echo "create elasticsearch plugins directory success"
+  else
+    echo "create elasticsearch plugins directory failed"
+  fi
+fi
+
+
+if [ -n "$(ls -A /usr/share/elasticsearch/plugins/analysis-ik)" ]; then
   echo "elasticsearch plugins directory is not empty"
 else
   echo "elasticsearch plugins directory is empty"
-  if cp -r /data/* /usr/share/elasticsearch/plugins; then
+  if cp -r /data/* /usr/share/elasticsearch/plugins/analysis-ik; then
     echo "copy elasticsearch plugins success"
   else
     echo "copy elasticsearch plugins failed"
